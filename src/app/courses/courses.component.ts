ffcb0ad88aef6fba0e8a2d16f4aa9bfa35d51879
AungMyoThuOC <thua40976@gmail.com>
2022-12-05 11:53:15 +0630

    alt done

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent {
+export class CoursesComponent{
+  constructor(
+    // private router: Router
+    private rout: Router
+  ) {}
 
+  ngOnInit(): void {
+
+  }
+
+  goTohtml(): void {
+    this.rout.navigate(['html']);
+  }
+
+  // goToInstructor(): void {
+  //   this.router.navigateByUrl('instructor');
+  // }
 }
