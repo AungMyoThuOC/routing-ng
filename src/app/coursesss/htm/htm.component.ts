3a15b48f8015ba94c39e2d3824f9518d761d9aee
AungMyoThuOC <thua40976@gmail.com>
2022-12-05 12:01:07 +0630

    alt done

--- src/app/coursesss/htm/htm.component.ts
+++ src/app/coursesss/htm/htm.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-htm',
   templateUrl: './htm.component.html',
   styleUrls: ['./htm.component.css']
 })
-export class HtmComponent {
+export class HtmComponent{
 
+  constructor(
+    private rou: Router
+  ) {}
+
+  ngOnInit(): void {
+
+  }
+
+  goToCourses(): void {
+    this.rou.navigate(['courses']);
+  }
 }
