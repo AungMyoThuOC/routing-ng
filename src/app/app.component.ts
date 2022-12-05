8c6e2a9e256f13504a79adeb6ae7eaa137392ba8
AungMyoThuOC <thua40976@gmail.com>
2022-12-05 16:51:57 +0630

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
 [x: string]: any;
   title = 'routing';
+
+  confirmBox() {
+    Swal.fire({
+      title: "Are you want to suer to watch",
+      text: "you don't leave this page",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes",
+      cancelButtonText: "No",
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire('Success!', 'You can see the page.', 'success');
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire('Cancelled', 'You can\t see the page', 'error');
+      }
+    });
+
+  }
 }
